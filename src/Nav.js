27e4208a011c920292b4624da81778f9c10f79d8
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2022-01-08 13:29:23 -0800

    Refactor home screen to question screen

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,12 @@
 import * as React from "react";
-import { View, Text } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import QuestionScreen from "./features/questions/QuestionScreen";
 
 const Stack = createNativeStackNavigator();
-
 function AppNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Question" component={QuestionScreen} />
     </Stack.Navigator>
   );
 }
